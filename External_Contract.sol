0bcf701d6f9199cf9ff01dd1bec1e31979c56068
sole <52155079+sole12343@users.noreply.github.com>
2022-05-30 15:17:00 +0800

    Create External_Contract.sol

---
+++ External_Contract.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+/*
+核心逻辑：Foo合约和Bar合约是可见的，Mal合约是隐藏的，
+把Mal合约地址传入Foo合约中，虽然调用的是callBar的函数，实际做的却是callMal的事情，障眼法而已。
+Let's say Alice can see the code of Foo and Bar but not Mal.
+It is obvious to Alice that Foo.callBar() executes the code inside Bar.log().
+However Eve deploys Foo with the address of Mal, so that calling Foo.callBar()
+will actually execute the code at Mal.
+*/
+
+/*
+1. Eve deploys Mal
+2. Eve deploys Foo with the address of Mal
+3. Alice calls Foo.callBar() after reading the code and judging that it is
+   safe to call.
+4. Although Alice expected Bar.log() to be execute, Mal.log() was executed.
+*/
+
+contract Foo {
+    Bar bar;
+
+    constructor(address _bar) {
+        bar = Bar(_bar);
+    }
+
+    function callBar() public {
+        bar.log();
+    }
+}
+
+contract Bar {
+    event Log(string message);
+
+    function log() public {
+        emit Log("Bar was called");
+    }
+}
+
+// This code is hidden in a separate file
+contract Mal {
+    event Log(string message);
+
+    // function () external {
+    //     emit Log("Mal was called");
+    // }
+
+    // Actually we can execute the same exploit even if this function does
+    // not exist by using the fallback
+    function log() public {
+        emit Log("Mal was called");
+    }
+}
